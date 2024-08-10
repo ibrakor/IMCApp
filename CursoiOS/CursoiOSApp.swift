5441f223ca71bb98e5d787de4232d77b3d365962
Ibra MAC <ibrahimkorsan@live.fr>
2024-08-10 23:27:21 +0200

    Initial Commit

---
+++ CursoiOS/CursoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoiOSApp.swift
+//  CursoiOS
+//
+//  Created by Brahim Korsan El Ghabi on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CursoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
